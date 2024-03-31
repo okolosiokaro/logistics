12c65854a49c1fd2d2dc9acb19c5104542e10732
Okolosiokaro <okolosiokaro@gmail.com>
2024-03-31 16:08:59 +0100

    latest change

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,12 +8,12 @@ const Testimonials = () => {
       <h3 className='text-2xl font-bold'>Latest News</h3>
       <div  className='grid grid-cols-2 my-8'>
       <div>
-        <img src='/' alt='news'/>
+        <img src='/' alt='news' className='mb-2'/>
         <p className='text-sm font-bold'>news feed</p>
         <span className='italic text-sm text-gray-400'>date</span>
       </div>
       <div>
-        <img src='/' alt='news'/>
+        <img src='/' alt='news' className='mb-2'/>
         <p className='text-sm font-bold'>news feed</p>
         <span className='italic text-sm text-gray-400'>date</span>
       </div>
@@ -22,11 +22,11 @@ const Testimonials = () => {
      
      <div>
      <h3 className='text-2xl font-bold'>Testimonials</h3>
-     <div className='flex my-8'>
+     <div className='flex my-8 gap-4'>
       <img src='/' alt='quotation mark'/>
       <div className='text-start'>
         <p className='text-gray-400 italic'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam.lorem20 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam.lorem20 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam.lorem20</p>
-        <div className='flex'>
+        <div className='flex mt-2'>
           <img src='/' alt='customer'/> 
           <div className=''>
             <p className='text-sm font-bold'>name</p>
